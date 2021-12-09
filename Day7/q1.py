28997f24841f632b1c7d999c761e3a485a2734d5
havess <haves.sam@gmail.com>
2021-12-08 22:33:02 -0500

    Cleanup

--- Day7/q1.py
+++ Day7/q1.py
@@ -4,25 +4,16 @@ initial_state = [int(x) for x in re.findall('\d+', input.read())]
 crabs = [initial_state.count(x) for x in range(max(initial_state) + 1)]
 cost = [0]*(max(initial_state)+1)
 crabs_so_far = crabs[0]
-last_cost = 0
-for i, c in enumerate(crabs):
-    if i == 0:
-        continue
-    cost[i] = last_cost + crabs_so_far
-    last_cost = cost[i]
-    if c:
-        crabs_so_far += c
+for i, c in enumerate(crabs[1:]):
+    cost[i+1] = cost[i] + crabs_so_far
+    crabs_so_far += c
 
 crabs_so_far = crabs[-1]
 last_cost = 0
-for i, c in reversed(list(enumerate(crabs))):
-    if i == len(crabs) - 1:
-        continue
-    temp = cost[i]
-    cost[i] += last_cost + crabs_so_far
-    last_cost = cost[i] - temp
-    if c:
-        crabs_so_far += c
+for i, c in reversed(list(enumerate(crabs[:-1]))):
+    last_cost = last_cost + crabs_so_far
+    cost[i] += last_cost
+    crabs_so_far += c
 
 print(min(cost))
 
--- Day7/q2.py
+++ Day7/q2.py
@@ -4,23 +4,16 @@ initial_state = [int(x) for x in re.findall('\d+', input.read())]
 crabs = [initial_state.count(x) for x in range(max(initial_state) + 1)]
 cost = [0]*(max(initial_state)+1)
 step_cost = num_crabs = crabs[0]
-last_cost = 0
-for i, c in enumerate(crabs):
-    if i == 0:
-        continue
-    cost[i] = last_cost + step_cost
-    last_cost = cost[i]
+for i, c in enumerate(crabs[1:]):
+    cost[i+1] = cost[i] + step_cost
     num_crabs += c
     step_cost += num_crabs
 
 step_cost = num_crabs = crabs[-1]
 last_cost = 0
-for i, c in reversed(list(enumerate(crabs))):
-    if i == len(crabs) - 1:
-        continue
-    temp = cost[i]
-    cost[i] += last_cost + step_cost
-    last_cost = (cost[i] - temp)
+for i, c in reversed(list(enumerate(crabs[:-1]))):
+    last_cost = last_cost + step_cost
+    cost[i] += last_cost
     num_crabs += c
     step_cost += num_crabs
 
